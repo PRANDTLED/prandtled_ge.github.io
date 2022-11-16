d3b20e7dc331e7c2a103718e71ca49b24ea83db1
PRANDTLED <38784540+PRANDTLED@users.noreply.github.com>
2022-11-16 20:10:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,31 +8,41 @@ tg.MainButton.text = "Გადარჩენა";
 tg.MainButton.show();
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
+  let message_ss = `?`;
+  let message_ss_deal = `:`;
+  let message_ss_type = `:`;
   let message = `?`;
   const city = document.getElementById("city");
   if (city.value === "1") {
     message += `Keyword=Тбилиси&cities=1996871&GID=1996871`;
+    message_ss += `MunicipalityId=95&CityIdList=95`;
   }
   if (city.value === "2") {
     message += `Keyword=Батуми&cities=8742159&GID=8742159`;
+    message_ss += `MunicipalityId=96&CityIdList=96`;
   }
   const deal = document.getElementById("deal");
   message += `&AdTypeID=${deal.value}`;
+  message_ss_deal += `${deal.value}:`;
   const type1 = document.getElementById("btn-check-outlined");
   if (type1.checked === true) {
     message += `&PrTypeID[]=1`;
+    message_ss_type += `/Flat`;
   }
   const type2 = document.getElementById("btn-check-2-outlined");
   if (type2.checked === true) {
     message += `&PrTypeID[]=2`;
+    message_ss_type += `/Private-House`;
   }
   const type3 = document.getElementById("btn-check-3-outlined");
   if (type3.checked === true) {
     message += `&PrTypeID[]=7`;
+    message_ss_type += `/Hotel`;
   }
   const type4 = document.getElementById("btn-check-4-outlined");
   if (type4.checked === true) {
     message += `&PrTypeID[]=4`;
+    message_ss_type += `/Commercial-Real-Estate`;
   }
   const currency1 = document.getElementById("success-outlined");
   if (currency1.checked === true) {
@@ -47,6 +57,7 @@ Telegram.WebApp.onEvent("mainButtonClicked", function () {
   if (pricefrom.value !== "") {
     if (priceto.value !== "") {
       message += `&FPriceFrom=${pricefrom.value}&FPriceTo=${priceto.value}`;
+      message_ss = `&PriceFrom=${pricefrom.value}&PriceTo=${priceto.value}`;
     }
   }
   const squarefrom = document.getElementById("squarefrom");
@@ -54,23 +65,31 @@ Telegram.WebApp.onEvent("mainButtonClicked", function () {
   if (squarefrom.value !== "") {
     if (squareto.value !== "") {
       message += `&AreaSizeFrom=${squarefrom.value}&AreaSizeTo=${squareto.value}`;
+      message_ss += `&QuantityFrom=${squarefrom.value}&QuantityTo=${squareto.value}`;
     }
   }
   const rooms1 = document.getElementById("btn-check-5-outlined");
   if (rooms1.checked === true) {
     message += `&RoomNums[]=1`;
+    message_ss += `&Rooms=1`;
   }
   const rooms2 = document.getElementById("btn-check-6-outlined");
   if (rooms2.checked === true) {
     message += `&RoomNums[]=2`;
+    message_ss += `&Rooms=2`;
   }
   const rooms3 = document.getElementById("btn-check-7-outlined");
   if (rooms3.checked === true) {
     message += `&RoomNums[]=3`;
+    message_ss += `&Rooms=3`;
   }
   const rooms4 = document.getElementById("btn-check-8-outlined");
   if (rooms4.checked === true) {
     message += `&RoomNums[]=4`;
-  }      
+    message_ss += `&Rooms=4`;
+  }
+  message += message_ss_type;
+  message += message_ss_deal;
+  message += message_ss;
   tg.sendData(message);
 });
