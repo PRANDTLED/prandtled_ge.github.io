f9d100e8e5e9429b1ad834d363cfb035ab4bc26d
PRANDTLED <38784540+PRANDTLED@users.noreply.github.com>
2022-12-01 23:47:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,22 +27,22 @@ Telegram.WebApp.onEvent("mainButtonClicked", function () {
   message_ss_deal += `${deal.value}:`;
   const type1 = document.getElementById("btn-check-outlined");
   if (type1.checked === true) {
-    message_mh_type += `/&PrTypeID[]=1`;
+    message_mh_type += `/&PrTypeID=1`;
     message_ss_type += `/bina`;
   }
   const type2 = document.getElementById("btn-check-2-outlined");
   if (type2.checked === true) {
-    message_mh_type += `/&PrTypeID[]=2`;
+    message_mh_type += `/&PrTypeID=2`;
     message_ss_type += `/kerdzo-saxli`;
   }
   const type3 = document.getElementById("btn-check-3-outlined");
   if (type3.checked === true) {
-    message_mh_type += `/&PrTypeID[]=7`;
+    message_mh_type += `/&PrTypeID=7`;
     message_ss_type += `/sastumro`;
   }
   const type4 = document.getElementById("btn-check-4-outlined");
   if (type4.checked === true) {
-    message_mh_type += `/&PrTypeID[]=4`;
+    message_mh_type += `/&PrTypeID=4`;
     message_ss_type += `/komerciuli`;
   }
   const currency1 = document.getElementById("success-outlined");
@@ -56,42 +56,46 @@ Telegram.WebApp.onEvent("mainButtonClicked", function () {
   const pricefrom = document.getElementById("pricefrom");
   const priceto = document.getElementById("priceto");
   if (pricefrom.value !== "") {
-    if (priceto.value !== "") {
-      message += `&FPriceFrom=${pricefrom.value}&FPriceTo=${priceto.value}`;
-      message_ss = `&PriceFrom=${pricefrom.value}&PriceTo=${priceto.value}`;
-    }
+    message += `&FPriceFrom=${pricefrom.value}`;
+    message_ss = `&PriceFrom=${pricefrom.value}`;
+  }
+  if (priceto.value !== "") {
+    message += `&FPriceTo=${priceto.value}`;
+    message_ss = `&PriceTo=${priceto.value}`;
   }
   const squarefrom = document.getElementById("squarefrom");
   const squareto = document.getElementById("squareto");
   if (squarefrom.value !== "") {
-    if (squareto.value !== "") {
-      message += `&AreaSizeFrom=${squarefrom.value}&AreaSizeTo=${squareto.value}`;
-      message_ss += `&QuantityFrom=${squarefrom.value}&QuantityTo=${squareto.value}`;
-    }
+    message += `&AreaSizeFrom=${squarefrom.value}`;
+    message_ss += `&QuantityFrom=${squarefrom.value}`;
+  }
+  if (squareto.value !== "") {
+    message += `&AreaSizeTo=${squareto.value}`;
+    message_ss += `&QuantityTo=${squareto.value}`;
   }
   const rooms1 = document.getElementById("btn-check-5-outlined");
   if (rooms1.checked === true) {
-    message += `&RoomNums[]=1`;
+    message += `&RoomNums=1`;
     message_ss += `&Rooms=1`;
   }
   const rooms2 = document.getElementById("btn-check-6-outlined");
   if (rooms2.checked === true) {
-    message += `&RoomNums[]=2`;
+    message += `&RoomNums=2`;
     message_ss += `&Rooms=2`;
   }
   const rooms3 = document.getElementById("btn-check-7-outlined");
   if (rooms3.checked === true) {
-    message += `&RoomNums[]=3`;
+    message += `&RoomNums=3`;
     message_ss += `&Rooms=3`;
   }
   const rooms4 = document.getElementById("btn-check-8-outlined");
   if (rooms4.checked === true) {
-    message += `&RoomNums[]=4`;
+    message += `&RoomNums=4`;
     message_ss += `&Rooms=4`;
   }
   const owner = document.getElementById("btn-check-9-outlined");
   if (owner.checked === true) {
-    message += `&OwnerTypeID[]=1`;
+    message += `&OwnerTypeID=1`;
     message_ss += `&IndividualEntityOnly=true`;
   }
   message += message_ss_type;
